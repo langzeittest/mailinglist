baf18791e5aad9035e7039b6bed85e023d99f362
oricgn <oliver@riesen.org>
2015-08-31 14:52:57 +0200

    iso-8859-1 language files

---
+++ lang/german.iso-8859-1.php
@@ -0,0 +1,14 @@
+<?php
+
+// This file ist part of the German Language Files Package
+// Get the complete package here:
+// http://www.phorum.org/phorum5/read.php?65,129717
+
+// Diese Datei ist Teil des Deutschen Sprachpakets
+// Das komplette Paket gibt es hier:
+// http://www.phorum.org/phorum5/read.php?65,129717
+
+$PHORUM['DATA']['LANG']['MailinglistMessage'] = "Forum: %forumname%\nBetreff: %subject%\nAutor: %author%\nLink zum Beitrag: %read_url%\n\n%plain_body%\n\n";
+$PHORUM['DATA']['LANG']['MailinglistSubject'] = '%forumname%: %subject%';
+
+?>
